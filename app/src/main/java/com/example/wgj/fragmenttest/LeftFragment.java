40a7e48329b23ac2138a52b380a901321b836b08
weiguangju__1994@163.com <weiguangju_1994@163.com>
2019-05-19 16:42:04 +0800

    init fragment

---
+++ app/src/main/java/com/example/wgj/fragmenttest/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.example.wgj.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by wgj on 2019/5/19.
+ */
+
+public class LeftFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/wgj/fragmenttest/RightFragment.java
@@ -0,0 +1,20 @@
+package com.example.wgj.fragmenttest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by wgj on 2019/5/19.
+ */
+
+public class RightFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        return view;
+    }
+}
